61d0be8439ef1d81743f523f21055fb5b73e5491
Osman Nuri Okumuş <onokumus@gmail.com>
2024-11-14 23:12:34 +0300

    v0.0.5

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -3,11 +3,11 @@ import { bicons } from "./bicons.js";
 export type BiIconName = keyof typeof bicons;
 
 export interface IconProps {
-    class?: string;
-    name: BiIconName;
-    font?: boolean;
-    size?: string | number;
-    fill?: string;
-    [key: string]: any;
+  class?: string;
+  name: BiIconName;
+  font?: boolean;
+  size?: string | number;
+  fill?: string;
+  [key: string]: any;
 }
 export { default as Bi } from "./Bi.svelte";
